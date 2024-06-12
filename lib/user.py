f3cc5b215cca93c1ef69f791938a9358869d2259
MartinWakaba <martin.wakaba@student.moringaschool.com>
2024-06-12 17:20:36 +0300

    instances made

--- lib/user.py
+++ lib/user.py
@@ -1,3 +1,31 @@
 class User:
     def __init__(self, id, name, email, phone_number):
-        pass
+        self._id = id
+        self._name = name
+        self._email = email
+        self._phone_number = phone_number
+
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string. ")
+        if len(value) == 0:
+            raise ValueError("Name cannot be empty!!!")
+        
+        self.name = value
+    
+    @property
+    def email(self):
+        return self._email
+    
+    @property
+    def phone_number(self):
+        return self._phone_number
